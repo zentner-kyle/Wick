05a021c15786c79e8e90ffa197812f1c97f23173
Kyle Zentner <zentner.kyle@gmail.com>
2013-07-23 00:35:14 -0700

    Removed unnecessary walloc functions

--- src/walloc.c
+++ src/walloc.c
@@ -1,33 +1,3 @@
-#include <wtype.h>
 #include <werror.h>
-#include <walloc.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-
-  /* For use in low level code. */
 
 werror wick_out_of_memory;
-
-void * walloc_static ( wtype * t, wcall * on_error ) {
-  void * result = wick_base_alloc ( t->size );
-  if ( result ) {
-    /* Hopefully makes finding bugs easier. */
-    memset ( result, 0xff, t->size );
-      }
-else {
-    winvoke ( on_error );
-      }
-  return result;
-    }
-
-void * wick_memalign ( size_t num_bytes, size_t alignment ) {
-  void * to_write;
-  assert ( ! ( alignment & ( alignment - 1 ) ) );
-  if ( posix_memalign ( &to_write, alignment, num_bytes ) ) {
-    return NULL;
-    }
-else {
-    return to_write;
-      }
-    }
--- src/walloc.h
+++ src/walloc.h
@@ -1,19 +1,8 @@
 #ifndef WALLOC_H
 #define WALLOC_H
-#include <wtype_h.h>
 #include <werror.h>
 
-#define wick_base_alloc( num_bytes ) malloc ( num_bytes )
-#define wick_base_free( ptr ) free ( ptr )
-#define wick_base_memalign( num_bytes, alignment ) wick_memalign ( num_bytes, alignment )
-
-void * wick_memalign ( size_t num_bytes, size_t alignment );
-
-#define WICK_ARENA_ALIGNMENT ( ( size_t ) ( 1 << 12 ) )
-
-#define walloc_simple( type, count ) ( ( type * ) wick_base_alloc ( sizeof ( type ) * count ) )
-
-void * walloc_static ( wtype * t, wcall * on_error );
+#define walloc_simple( type, count ) ( ( type * ) malloc ( sizeof ( type ) * count ) )
 
 extern werror wick_out_of_memory;
 
